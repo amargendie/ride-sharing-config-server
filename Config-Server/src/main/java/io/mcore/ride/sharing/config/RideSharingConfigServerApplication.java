19d6d4bc8839588de9dc83ad6ba518940e335424
Andres Margendie Benoit <machandi@gmail.com>
2016-09-25 11:29:31 +0200

    Initial Commit

---
+++ Config-Server/src/main/java/io/mcore/ride/sharing/config/RideSharingConfigServerApplication.java
@@ -0,0 +1,12 @@
+package io.mcore.ride.sharing.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingConfigServerApplication.class, args);
+	}
+}
---
+++ Config-Server/src/test/java/io/mcore/ride/sharing/config/RideSharingConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mcore.ride.sharing.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RideSharingConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
